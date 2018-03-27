e53038143940fb3db57c40f70ce41738fb776e39
Wolfereign <Wolfereign@users.noreply.github.com>
2018-03-26 22:24:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@
 #rm ~/.vimrc
 #rm ~/.screenrc
 #rm ~/.tmux.conf
-#ln -s dotfiles/.bashrc ~/
-#ln -s dotfiles/.bash_aliases ~/
-#ln -s dotfiles/.bash_exports ~/
-#ln -s dotfiles/.vimrc ~/
-#ln -s dotfiles/.screenrc ~/
-#ln -s dotfiles/.tmux.conf ~/
+#ln -s .dotfiles/.bashrc ~/
+#ln -s .dotfiles/.bash_aliases ~/
+#ln -s .dotfiles/.bash_exports ~/
+#ln -s .dotfiles/.vimrc ~/
+#ln -s .dotfiles/.screenrc ~/
+#ln -s .dotfiles/.tmux.conf ~/

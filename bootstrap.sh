950de61bb15688dad89b4f1557988c115c9be288
Wolfereign <nvonwolff@gmail.com>
2018-04-05 20:12:56 -0600

    refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,146 +2,55 @@
 #
 # bootstrap installs things.
 
+# Set dotfile base directory as current
 cd "$(dirname "$0")"
 DOTFILES_ROOT=$(pwd -P)
 
+# Stop Script on Error
 set -e
 
-echo ''
+# Create/Refresh dotfile symlinks (~/*)
+for src in $(find -H "$DOTFILES_ROOT" -maxdepth 4 -name '*.symh' -not -path '*.git*')
+do
+  dst="$HOME/.$(basename "${src%.*}")"
+  rm -rf "$dst" || true
+  ln -s "$src" "$dst"
+done
 
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
+# Create/Refresh dotfile symlinks (~/.config/*)
+mkdir ~/.config &>/dev/null || true
+for src in $(find -H "$DOTFILES_ROOT" -maxdepth 4 -name '*.symc' -not -path '*.git*')
+do
+  dst="$HOME/.config/$(basename "${src%.*}")"
+  rm -rf "$dst" || true
+  ln -s "$src" "$dst"
+done
 
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
+# Setup Local User Git Info if Missing
+if ! [ -f git/userinfo ]; then
+  info 'setup user info'
 
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
+  touch git/userinfo
 
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
+  user ' - What is your github author name?'
+  read -er git_authorname
+  user ' - What is your github author email?'
+  read -er git_authoremail
 
-setup_gituserinfo () {
-  if ! [ -f git/userinfo ]
-  then
-    info 'setup user info'
+  echo "[user]" > git/userinfo
+  echo "name = ${git_authorname}" >> git/userinfo
+  echo "email = ${git_authoremail}" >> git/userinfo
 
-		touch git/userinfo
-
-    user ' - What is your github author name?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
-
-		echo "[user]" > git/userinfo
-		echo "name = ${git_authorname}" >> git/userinfo
-		echo "email = ${git_authoremail}" >> git/userinfo
-
-    success 'gitconfig'
-  fi
-}
-
-
-link_file () {
-  local src=$1 dst=$2
-
-  local overwrite= backup= skip=
-  local action=
-
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-  then
-
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
-    then
-
-      local currentSrc="$(readlink $dst)"
-
-      if [ "$currentSrc" == "$src" ]
-      then
-
-        skip=true;
-
-      else
-
-        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
-        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
-        read -n 1 action
-
-        case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
-        esac
-
-      fi
-
-    fi
-
-    overwrite=${overwrite:-$overwrite_all}
-    backup=${backup:-$backup_all}
-    skip=${skip:-$skip_all}
-
-    if [ "$overwrite" == "true" ]
-    then
-      rm -rf "$dst"
-      success "removed $dst"
-    fi
-
-    if [ "$backup" == "true" ]
-    then
-      mv "$dst" "${dst}.backup"
-      success "moved $dst to ${dst}.backup"
-    fi
-
-    if [ "$skip" == "true" ]
-    then
-      success "skipped $src"
-    fi
-  fi
-
-  if [ "$skip" != "true" ]  # "false" or empty
-  then
-    ln -s "$1" "$2"
-    success "linked $1 to $2"
-  fi
-}
-
-install_dotfiles () {
-  info 'installing dotfiles'
-
-  local overwrite_all=false backup_all=false skip_all=false
-
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 3 -name '*.symlink' -not -path '*.git*')
-  do
-    dst="$HOME/.$(basename "${src%.*}")"
-    link_file "$src" "$dst"
-  done
-}
-
-setup_gituserinfo
-install_dotfiles
+  success 'gitconfig'
+fi
 
 # Setup SSH Files
-touch ~/.ssh/config
-chmod 600 ~/.ssh/config
-echo "AddKeysToAgent yes" >> ~/.ssh/config
+if [ ! -f ~/.ssh/config ]; then
+  mkdir ~/.ssh &>/dev/null || true
+  touch ~/.ssh/config
+  chmod -R 700 ~/.ssh
+  echo "AddKeysToAgent yes" >> ~/.ssh/config
+fi
 
 # Install Vim-Plug & Plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

b8a3dd5939ab4018f8a15d1723299753686ae68a
Wolfereign <nvonwolff@gmail.com>
2018-04-08 02:06:11 -0600

    fonts!

--- scripts/tumbleweed-i3-setup.sh
+++ scripts/tumbleweed-i3-setup.sh
@@ -16,12 +16,9 @@ zypper install -y lightdm-slick-greeter-branding-upstream
 sed -i  's#\(DISPLAYMANAGER=\)\(.*\)#\1"lightdm"#' /etc/sysconfig/displaymanager
 
 # Install i3 and required apps for my config
-zypper install -y i3-gaps i3lock rofi compton alacritty scrot feh ImageMagick
+zypper install -y i3-gaps i3lock rofi compton alacritty scrot feh ImageMagick pango
 sed -i  's#\(DEFAULT_WM=\)\(.*\)#\1"i3"#' /etc/sysconfig/windowmanager
 ln -sfn /usr/share/xsessions/i3.desktop /etc/alternatives/default-xsession.desktop
-
-# Install Fonts
-zypper install -y pango google-roboto-fonts google-roboto-mono-fonts
 
 # Install Polybar
 zypper ar https://download.opensuse.org/repositories/home:/sysek/openSUSE_Tumbleweed/home:sysek.repo
@@ -35,4 +32,12 @@ zypper install -y pulseaudio pulseaudio-utils alsa-plugins-pulse pulseaudio-modu
 
 # Install my preffered apps
 zypper install -y vim tmux ranger git unzip firefox 
+
+# Install KVM
+zypper install install -y -t pattern kvm_server kvm_tools
+
+# Install Docker
+zypper install -y docker docker-compose
+systemctl enable docker
+systemctl start docker
 

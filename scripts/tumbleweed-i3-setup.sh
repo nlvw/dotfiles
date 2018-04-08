d70a9dab09282eee4ac2c84041a9b73f7db27b3c
Wolfereign <nvonwolff@gmail.com>
2018-04-08 02:39:27 -0600

    clipit

--- scripts/tumbleweed-i3-setup.sh
+++ scripts/tumbleweed-i3-setup.sh
@@ -30,14 +30,19 @@ zypper install -y pulseaudio pulseaudio-utils alsa-plugins-pulse pulseaudio-modu
 
 # pulseaudio-ctl?
 
-# Install my preffered apps
-zypper install -y vim tmux ranger git unzip firefox 
-
 # Install KVM
-zypper install install -y -t pattern kvm_server kvm_tools
+zypper install install -y -t pattern kvm_server
 
 # Install Docker
 zypper install -y docker docker-compose
 systemctl enable docker
 systemctl start docker
 
+# Install my preffered apps
+zypper install -y vim tmux ranger git unzip clipit firefox 
+
+# Install visual studio code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+sudo zypper refresh
+sudo zypper -y install code

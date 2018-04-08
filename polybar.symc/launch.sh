a44479bf96a9c3d181454c077e64c3cf69cbb635
Wolfereign <nvonwolff@gmail.com>
2018-04-08 00:31:17 -0600

    top bar

--- polybar.symc/launch.sh
+++ polybar.symc/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bottom -c ~/.config/polybar/config &
+polybar top -c ~/.config/polybar/config &
 
 echo "Bars launched..."

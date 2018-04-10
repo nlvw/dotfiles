b9e5c1b31734c5d52f534195d565540c36a31cd0
Wolfereign <Wolfereign@users.noreply.github.com>
2018-04-10 12:33:02 -0600

    Create chrooted-setup.sh

---
+++ scripts/archlinux/chrooted-setup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# This Will Automate the Arch Linux Setup Steps After the user has installed and chrooted into the new installation
+# UEFI and systemd-boot are default/assumed
+
+# Kill script on error
+set -e
+
+# Set Current Directory to root home
+cd ~
+
+########################################################################################
+# Get/Set Information From User
+########################################################################################
+
+# Hostname
+read -rep "Desired Hostname: " hName
+
+# Root Password
+echo "Setting Root Password.  Please Input Desired Password: "
+passwd
+
+# New Admin User
+read -rep "New Admin Desired User Name: " uName
+useradd -m "$uName" -G wheel
+echo "Setting $uName Password. Please Input Desired Password: "
+passwd "$uName"
+
+
+########################################################################################
+# Setup Bootloader
+########################################################################################
+
+# Set ESP/Boot directory
+bootctl --path=/boot install
+
+# Call "bootctl update" when systemd is updated
+mkdir /etc/pacman.d/hooks || true
+cat << EOF > /etc/pacman.d/hooks/systemd-boot.hook
+[Trigger]
+Type = Package
+Operation = Upgrade
+Target = systemd
+
+[Action]
+Description = Updating systemd-boot
+When = PostTransaction
+Exec = /usr/bin/bootctl update
+EOF
+
+# Set Loader Configuration
+cat << EOF > /etc/boot/loader/loader.conf
+default arch
+timeout 3
+editor 0
+console-mode auto
+EOF
+
+# Create Boot Entry
+set puID = findmnt / -o PARTUUID -n
+mkdir /boot/loader/entries || true
+cat << EOF > /etc/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=PARTUUID=$puID rw
+EOF
+
+# Update boot partition just in case
+bootctl update
+
+########################################################################################
+# Setup Locale/Time
+########################################################################################
+
+# Time Zone
+ln -sf /usr/share/zoneinfo/US/Mountain /etc/localtime
+hwclock --systohc
+
+# Locale
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=us > /etc/vconsole.conf
+locale-gen
+
+########################################################################################
+# Hostname
+########################################################################################
+echo "$hName" > /etc/hostname
+cat << EOF >> /etc/hosts
+127.0.0.1	localhost
+::1		    localhost
+127.0.1.1	${hName}.localdomain	hName
+EOF
+
+########################################################################################
+# Pull Git Repo
+########################################################################################
+pacman -S --noconfirm git
+git clone https://github.com/Wolfereign/.dotfiles

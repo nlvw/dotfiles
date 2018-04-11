7ccc49d3a2c19d843d90715614177ddbdd0bfacb
Wolfereign <nvonwolff@gmail.com>
2018-04-11 13:27:10 -0600

    sed redo

--- scripts/os_setup/tumbleweed-setup-i3.sh
+++ scripts/os_setup/tumbleweed-setup-i3.sh
@@ -3,12 +3,19 @@
 # Setup For i3-gaps on OpenSuse Tumbleweed
 # Server / Text Only Install
 
+# Stop on Error
+set -e
+
 # Disable automatic installation of reccomended packages
-sed -i  's#\(solver.onlyRequires = \)\(.*\)#\1false#' /etc/zypp/zypp.conf
+sed -i  '/solver.onlyRequires/c\solver.onlyRequires = true' /etc/zypp/zypp.conf
 
 # Check for, and apply, updates
 zypper ref
 zypper dup -y
+
+# Install GPU Driver
+zypper addrepo --refresh http://http.download.nvidia.com/opensuse/tumbleweed
+zypper inr -r NVIDIA
 
 # Install x11 (X Windows System)
 zypper install -y -t pattern x11
@@ -16,20 +23,28 @@ systemctl set-default graphical
 
 # Install/Configure Display Manager
 zypper install -y lightdm-slick-greeter-branding-upstream
-sed -i  's#\(DISPLAYMANAGER=\)\(.*\)#\1"lightdm"#' /etc/sysconfig/displaymanager
+sed -i '/DISPLAYMANAGER=/c\DISPLAYMANAGER="lightdm"' /etc/sysconfig/displaymanager
+#sed -i  's#\(DISPLAYMANAGER=\)\(.*\)#\1"lightdm"#' /etc/sysconfig/displaymanager
 
 # Install i3 and required apps for my config
 zypper install -y i3-gaps i3lock rofi compton alacritty scrot feh ImageMagick pango
-sed -i  's#\(DEFAULT_WM=\)\(.*\)#\1"i3"#' /etc/sysconfig/windowmanager
+sed -i '/DEFAULT_WM=/c\DEFAULT_WM="i3"' /etc/sysconfig/windowmanager
+#sed -i  's#\(DEFAULT_WM=\)\(.*\)#\1"i3"#' /etc/sysconfig/windowmanager
 ln -sfn /usr/share/xsessions/i3.desktop /etc/alternatives/default-xsession.desktop
 
 # Install Polybar
-zypper ar https://download.opensuse.org/repositories/home:/sysek/openSUSE_Tumbleweed/home:sysek.repo
-zypper --gpg-auto-import-keys refresh
+zypper ar -f https://download.opensuse.org/repositories/home:/sysek/openSUSE_Tumbleweed/home:sysek.repo
+zypper --gpg-auto-import-keys refresh -f -r https://download.opensuse.org/repositories/home:/sysek/openSUSE_Tumbleweed/home:sysek.repo
 zypper install -y polybar
 
 # Install Audio -- pulseaudio-ctl?
 zypper install -y pulseaudio pulseaudio-utils alsa-plugins-pulse pulseaudio-module-zeroconf pulseaudio-module-x11 pavucontrol
+
+# Install Mulimedia Codecs
+zypper ar -f http://packman.inode.at/suse/openSUSE_Tumbleweed/ packman
+zypper ar -f http://opensuse-guide.org/repo/openSUSE_Tumbleweed/ libdvdcss
+zypper ref
+zypper install -y libdvdcss2 ffmpeg lame gstreamer-plugins-libav gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon vlc vlc-codecs flash-player flash-player-ppapi libxine2 libxine2-codecs
 
 # Install KVM
 zypper install install -y -t pattern kvm_server

43dda5112882c5f1a48eff04c0153b7ce4b21e62
Wolfereign <nvonwolff@gmail.com>
2018-04-11 14:20:35 -0600

    root check

--- scripts/os_setup/tumbleweed-setup-i3.sh
+++ scripts/os_setup/tumbleweed-setup-i3.sh
@@ -3,6 +3,12 @@
 # Setup For i3-gaps on OpenSuse Tumbleweed
 # Server / Text Only Install
 
+# Ensure script is being run as root 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Stop on Error
 set -e
 
